1c8ad5f53a2f3c679c2c80e121d8aa4d48636284
mindelias@gmail.com <mindelias@gmail.com>
2021-06-29 22:33:33 +0100

    merge

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -29,22 +29,6 @@ export interface MixedCardProps {
   secondTextColor?: string;
 }
 
-export interface MixedCardProps {
-  bgColor?: string;
-  textColor?: string;
-  width?: string;
-  icon1?: string;
-  icon2?: string;
-  icon3?: string;
-  height?: String;
-  title?: string;
-  amount?: string | number;
-  borderColor?: string;
-  numberColor?: string;
-  text?: string;
-  secondTextColor?: string;
-}
-
 export const UpIconCards = ({
   bgColor,
   textColor,
@@ -158,47 +142,6 @@ export const MixedIcons = ({
   );
 };
 
-export const MixedIcons = ({
-  bgColor,
-  textColor,
-  width,
-  icon1,
-  icon2,
-  icon3,
-  height,
-  title,
-  amount,
-  borderColor,
-  numberColor,
-  text,
-  secondTextColor
-}: MixedCardProps) => {
-  return (
-    <MixedStyle
-      bgColor={bgColor}
-      borderColor={borderColor}
-      textColor={textColor}
-      numberColor={numberColor}
-      secondTextColor={secondTextColor}
-    >
-      <div className="flex-container">
-        <div className="title">{title}</div>
-        <div>
-          <img className="icon1" src={icon1} alt="" />
-        </div>
-        <div className="amount-txt">{amount}</div>
-        <div className="secondTxt">{text}</div>
-      </div>
-      <div>
-        <span className="last-images">
-          <img src={icon2} alt="" />
-          <img className="ml-2" src={icon3} alt="" />
-        </span>
-      </div>
-    </MixedStyle>
-  );
-};
-
 const UpIconCardStyle = styled.div`
   border-radius: 10px;
   padding: 1rem 1rem;
@@ -278,48 +221,6 @@ const MixedStyle = styled.div`
     margin-bottom: 0;
   }
   .secondTxt {
-    font-weight: 400;
-    font-size: 1.1rem;
-    margin-bottom: 0;
-    color: ${(props: MixedCardProps) => props.secondTextColor};
-  }
-`;
-
-const MixedStyle = styled.div`
-  border-radius: 10px;
-
-  padding: 1rem 1rem;
-
-  background-color: ${(props: MixedCardProps) => props.bgColor};
-  border: ${(props: MixedCardProps) => props.borderColor};
-  height: 135px;
-  min-width: 200px;
-  margin-bottom: 30px;
-  .icon1 {
-    float: right;
-  }
-  .flex-container {
-    display: flex;
-    height: 85%;
-    flex-direction: column;
-    gap: 0.3rem;
-  }
-  .last-images {
-    float: right;
-  }
-  .title {
-    font-weight: 400;
-    font-size: 1.1rem;
-    margin-bottom: 0;
-    color: ${(props: MixedCardProps) => props.textColor};
-  }
-  .amount-txt {
-    font-size: 1.5rem;
-    font-weight: 600;
-    color: ${(props: MixedCardProps) => props.numberColor};
-    margin-bottom: 0;
-  }
-  .secondTxt{
     font-weight: 400;
     font-size: 1.1rem;
     margin-bottom: 0;
