70dc673bc3029e541776f9bf214693b9a3b85eec
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2021-10-21 02:18:36 +0100

    a

--- scripts/install-app.sh
+++ scripts/install-app.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /var/www/react/test/admin-frontend
-npm install --save --save-exact 
-npm config set save-exact=true
+npm install
 npm i react-chartjs-2@3.0.3
--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /var/www/react/test/admin-frontend
+npm i react-chartjs-2@3.0.3
 npm run build
